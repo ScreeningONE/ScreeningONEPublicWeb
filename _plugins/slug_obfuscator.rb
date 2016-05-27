3fd0233d44a3e586012adc57102f0f5ef7b9a89f
Jung Oh <jung@jungoh.me>
2016-05-27 15:14:25 -0500

    changed url from using title to slug that is grabbed from contentful

--- _plugins/slug_obfuscator.rb
+++ _plugins/slug_obfuscator.rb
@@ -1,8 +1,7 @@
 class SlugObfuscator < ::Jekyll::Contentful::Mappers::Base
   def map
     result = super
-    slug = result['title'].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-    result['permalink'] = "proposals/#{slug}-#{result['sys']['id']}/index.html"
+    result['permalink'] = "proposals/#{result['slug']}-#{result['sys']['id']}/index.html"
     result
   end
 end
