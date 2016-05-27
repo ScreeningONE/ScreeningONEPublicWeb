86aaf87ea5ab88e07db7d95be679eb3aa846f776
Stephen Crosby <stevecrozz@gmail.com>
2016-05-26 17:15:23 -0700

    add /index.html to proposals

--- _plugins/slug_obfuscator.rb
+++ _plugins/slug_obfuscator.rb
@@ -1,7 +1,7 @@
 class SlugObfuscator < ::Jekyll::Contentful::Mappers::Base
   def map
     result = super
-    result['permalink'] = "proposals/#{result['slug']}-#{result['sys']['id']}"
+    result['permalink'] = "proposals/#{result['slug']}-#{result['sys']['id']}/index.html"
     result
   end
 end
